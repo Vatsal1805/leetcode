9ec4dd0da597c8f7a66c578f0517ccff286eb07e
Vatsal Bhavsasr <144819478+Vatsal1805@users.noreply.github.com>
2025-06-01 18:46:01 +0530

    Time: 2 ms (58.13%) | Memory: 9.8 MB (26.58%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>count1;
+        unordered_map<char,int>count2;
+
+        for(char ch:s)
+        count1[ch]++;
+
+        for(char ch:t)
+        count2[ch]++;
+
+        if(count1==count2)
+        return true;
+        else
+        return false;
+
+    }
+};
