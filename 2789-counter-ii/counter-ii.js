a5e3cb0a31640afc0dc0593ac05ebeb1b943140f
Vatsal Bhavsasr <144819478+Vatsal1805@users.noreply.github.com>
2025-12-11 20:57:53 +0530

    Time: 50 ms (70.51%) | Memory: 56.7 MB (36.42%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current=init
+    return{
+      increment:()=>{
+        
+         current++;
+         return current;
+      },
+      decrement:()=>{
+
+         current--;
+         return current;
+      },
+      reset:(val)=>{
+        return current=init;
+      }
+
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
